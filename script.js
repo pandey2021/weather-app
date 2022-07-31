e94411f59cd352e4106d111f0dab2020d6cacff4
pandey2021 <84921465+pandey2021@users.noreply.github.com>
2022-07-31 21:56:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let weather = {
+    "apikey": "efb95ba5f095d4e6d2450235b047b080",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        + "&units=metric&appid=" 
+        + this.apikey)
+        .then((response)=> response.json())
+        .then((data) => this.displayweather(data));
+
+    },
+    displayweather:function(data){
+        const { name } = data;
+        const { icon , description } = data.weather[0];
+        const { temp, humidity, pressure, feels_like } = data.main;
+        const { speed } = data.wind;
+        let intpressure = parseInt(pressure);
+        intpressure = pressure / 1000;
+        console.log(intpressure);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".feels").innerText = "Feels like: " + feels_like + "°C";
+        document.querySelector(".pressure").innerText = "Pressure: " 
+        + intpressure.toString() + "atm";
+        document.querySelector(".humidity").innerText =
+          "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function() {
+        weather.search();
+    }
+);
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function(event) {
+        if(event.key == "Enter"){
+            weather.search();
+        }
+});
+weather.fetchWeather("Delhi");
